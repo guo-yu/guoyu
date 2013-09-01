b346ec0f896074594d465297ec774c4de1f4174a
Guo Yu <i@guoyu.me>
2013-09-01 23:18:17 +0800

    hello everyone

---
+++ README.md
@@ -0,0 +1,20 @@
+GuoYu ![npm](https://badge.fury.io/js/guoyu.png)
+---
+
+let me introduce myself
+
+### How to install
+````
+$ npm install guoyu
+````
+
+### Sample code
+
+````javascript
+var guoyu = require('guoyu');
+
+console.log(guoyu.birthday); // prints Mon Mar 04 1991 00:00:00 GMT+0800 (HKT)
+console.log(guoyu.age); // prints 22
+console.log(guoyu.github); // prints 'http://github.com/turingou'
+console.log(guoyu.avatar); // prints 'http://tp4.sinaimg.cn/1644105187/180/5621090715/1'
+````
---
+++ index.js
@@ -0,0 +1,16 @@
+var guoyu = {
+    gender: 'male',
+    birthday: new Date('1991/03/04'),
+    age: new Date().getFullYear() - new Date('1991/03/04').getFullYear(),
+    avatar: 'http://tp4.sinaimg.cn/1644105187/180/5621090715/1',
+    mobile: '+8613760120683',
+    email: 'i@guoyu.me',
+    location: 'ShenZhen, Mainland China',
+    homepage: 'http://guoyu.me',
+    github: 'http://github.com/turingou',
+    npm: 'https://npmjs.org/~turing',
+    weibo: 'http://weibo.com/turingou',
+    v2ex: 'http://v2ex.com/member/turing'
+}
+
+module.exports = guoyu;

98ad7bf5c4b263f508d598c3d560a4e6c34f2c2c
Guo Yu <i@guoyu.me>
2014-08-18 20:25:19 +0800

    0.1: update infomation

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-var guoyu = {
-    gender: 'male',
-    birthday: new Date('1991/03/04'),
-    age: new Date().getFullYear() - new Date('1991/03/04').getFullYear(),
-    avatar: 'http://tp4.sinaimg.cn/1644105187/180/5621090715/1',
-    mobile: '+8613760120683',
-    email: 'i@guoyu.me',
-    location: 'ShenZhen, Mainland China',
-    homepage: 'http://guoyu.me',
-    github: 'http://github.com/turingou',
-    npm: 'https://npmjs.org/~turing',
-    weibo: 'http://weibo.com/turingou',
-    v2ex: 'http://v2ex.com/member/turing'
-}
-
-module.exports = guoyu;
+module.exports = {
+  gender: 'male',
+  birthday: new Date('1991/03/04'),
+  age: new Date().getFullYear() - new Date('1991/03/04').getFullYear(),
+  avatar: 'http://tp4.sinaimg.cn/1644105187/180/5621090715/1',
+  mobile: '08613760120683',
+  homepage: 'http://guoyu.me',
+  email: {
+    working: 'o.u.turing@gmail.com',
+    private: 'i@guoyu.me'
+  },
+  blog: {
+    uri: 'http://guoyu.me',
+    engine: 'airpub'
+  },
+  location: {
+    homeland: 'Mainland China',
+    home: 'ShenZhen',
+    current: 'Peking'
+  },
+  github: {
+    id: 'turingou',
+    uri: 'http://github.com/turingou'
+  }
+  npm: {
+    id: 'turing',
+    uri: 'https://npmjs.org/~turing'
+  }
+  weibo: {
+    id: 'turingou',
+    uri: 'http://weibo.com/turingou'
+  }
+  v2ex: {
+    id: 'turing',
+    uri: 'http://v2ex.com/member/turing'
+  }
+};
